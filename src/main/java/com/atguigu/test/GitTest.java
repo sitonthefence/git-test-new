bbac8972c707cec1d6a3e4d9042eb42da1c8a27f
jiangyang <591277686@qq.com>
2023-09-03 09:06:17 +0800

    master commit4

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello,git!!3" );
         System.out.println("hello,git!!4，hotfix" );
         System.out.println("hello,git!!4，master" );
+        System.out.println("hotfix-test" );
     }
 }
